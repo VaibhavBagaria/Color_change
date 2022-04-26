e3e7ce8304a010875693133d2c4817564451579f
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2022-04-26 10:18:00 -0300

    Add files via upload

--- Color trick game.py
+++ Color trick game.py
@@ -6,6 +6,9 @@ root.geometry("600x600")
 
 color_l=Label(root,font=('times bold',30))
 color_l.place(relx=0.4,rely=0.4,anchor=CENTER)
+score_l=Label(root,text="Score:0",font=('times bold',20))
+score_l.place(relx=0.1,rely=0.05,anchor=CENTER)
+input_value=Entry(root)
 
 class Game():
     def __init__(self):
@@ -19,13 +22,28 @@ class Game():
             self.random_text=random.randint(0,7)
             
             color_l['text']=self.color_text[self.random_text]
-            color_l['fg']=self.color_text[self.random_color]
+            color_l['fg']=self.color_name[self.random_color]
             
             btn1['text']="Next"
             
+    def __update_score(self,input_value):
+        if input_value==self.color_name[self.random_color]:
+            self.score+=random.randint(1,10)
+            score_l['text']="Score: "+self.score
+            
+    def get_user_value(self,input_value):
+        __update_score(self,input_value)
+        
+obj_game=Game()
+def getInput() :
+    output=input_value.get()
+    obj_game.get_user_value(output)
+    obj_game.update_score()
+    input_value.delete(0,END)
+            
 obj_game=Game()
 
-btn1=Button(root,text="Start",command=obj_game.update_game)
+btn1=Button(root,text="Start",command=getInput,relief=FLAT)
 btn1.place(relx=0.5,rely=0.7,anchor=CENTER)
 
 root.mainloop()
