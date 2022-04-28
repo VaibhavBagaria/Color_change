3eed4614612444cf5919dc806f65daf1e53bac51
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2022-04-28 08:16:10 -0300

    Add files via upload

--- Color trick game.py
+++ Color trick game.py
@@ -1,49 +1,46 @@
-from tkinter import*
+from tkinter import *
 import random
-root=Tk()
+
+root = Tk()
 root.title("Encapsulation")
-root.geometry("600x600")
+root.geometry("500x400")
+root.config(bg="white")
+label_score = Label(root, text="Score : 0",font=("Bahnschrift Light",15,"bold") ,bg="white")
+label_score.place(relx=0.1,rely=0.1, anchor= CENTER)
+label_name = Label(root,font=("Arial",40),bg="white")
+label_name.place(relx=0.5,rely=0.3, anchor= CENTER)
+input_value = Entry(root)
+input_value.place(relx=0.5,rely=0.5, anchor= CENTER)
 
-color_l=Label(root,font=('times bold',30))
-color_l.place(relx=0.4,rely=0.4,anchor=CENTER)
-score_l=Label(root,text="Score:0",font=('times bold',20))
-score_l.place(relx=0.1,rely=0.05,anchor=CENTER)
-input_value=Entry(root)
-
-class Game():
+class game:
     def __init__(self):
-        self.score=0
-    
-    def update_game(self):
-            self.color_name=["blue","aqua","green","yellow","red","pink","purple","black"]
-            self.random_color=random.randint(0,7)
-            
-            self.color_text=["Blue","Aqua","Green","Yellow","Red","Pink","Purple","Black"]
-            self.random_text=random.randint(0,7)
-            
-            color_l['text']=self.color_text[self.random_text]
-            color_l['fg']=self.color_name[self.random_color]
-            
-            btn1['text']="Next"
-            
-    def __update_score(self,input_value):
-        if input_value==self.color_name[self.random_color]:
-            self.score+=random.randint(1,10)
-            score_l['text']="Score: "+self.score
-            
-    def get_user_value(self,input_value):
-        __update_score(self,input_value)
+        self.__score = 0
+    def updateGame(self):
+        self.text = ["BLACK","PINK","GREEN","BLUE","YELLOW","RED"]
+        self.random_number_for_text = random.randint(0,5)
+        self.color = ["black","pink","green","blue","yellow","red"]
+        self.random_number_for_color = random.randint(0,5)
+        label_name["text"] =  self.text[self.random_number_for_text]
+        label_name["fg"] = self.color[self.random_number_for_color]
         
-obj_game=Game()
-def getInput() :
-    output=input_value.get()
-    obj_game.get_user_value(output)
-    obj_game.update_score()
-    input_value.delete(0,END)
+    def __update_score(self,input_value):
+        if(input_value == self.color[self.random_number_for_color]):
+            print(self.color[self.random_number_for_color])
+            self.__score = self.__score + random.randint(0,10)
+            label_score["text"] = "Score : "+str(self.__score)
             
-obj_game=Game()
-
-btn1=Button(root,text="Start",command=getInput,relief=FLAT)
-btn1.place(relx=0.5,rely=0.7,anchor=CENTER)
-
+    def get_user_value(self, input_value):
+        self.__update_score(input_value)
+        
+obj = game()
+def getInput():
+    value = input_value.get()
+    obj.get_user_value(value)
+    obj.updateGame()
+    input_value.delete(0,END)
+    
+btn = Button(root, text="CHECK" ,command=getInput, bg="IndianRed1", fg="white", relief=FLAT,  padx=10, pady=1,  font=("Arial",15))
+btn.place(relx=0.35,rely=0.65, anchor= CENTER)
+btn = Button(root, text="START" ,command=obj.updateGame, bg="dark olive green", fg="white", relief=FLAT,  padx=10, pady=1,  font=("Arial",15))
+btn.place(relx=0.65,rely=0.65, anchor= CENTER)
 root.mainloop()
